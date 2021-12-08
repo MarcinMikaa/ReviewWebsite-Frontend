5af21ba673440e3f6cae465a48acf1a479b2b917
Marcin <marcin.mika123@gmail.com>
2021-12-08 20:14:43 +0100

    Navigation added succesfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 function App() {
   return (
     <Router>
-      <div className="App"></div>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<MainPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
