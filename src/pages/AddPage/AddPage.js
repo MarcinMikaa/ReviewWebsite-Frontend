8c259871474168e01c2f22474f83f3d92b419771
Jakub Rzepiel <jakub.rzepiel@uczen.ckziu.net>
2022-02-09 20:58:00 +0100

    updated the form

--- src/pages/AddPage/AddPage.js
+++ src/pages/AddPage/AddPage.js
@@ -1,22 +1,28 @@
 import "./AddPage.css";
 import { Form, Button } from "react-bootstrap";
 
+
+
 function AddPage() {
+
   return (
     <div className="add-page">
       <div className="form-box">
-        <h1>Add new shoe</h1>
+        <h1>Add new review</h1>
         <Form>
-          <Form.Group className="mb-3" controlId="formGroupUsername">
-            <Form.Control type="text" placeholder="Name" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Group className="mb-3" controlId="formGroupTitle">
             <Form.Control type="text" placeholder="Title" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupPassword">
-            <Form.Control type="date" placeholder="Content" />
+          <Form.Group className="mb-7" controlId="formGroupContent">
+            <Form.Control type="text" placeholder="Content" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Group className="mb-3" controlId="formGroupDate">
+            <Form.Control type="date" placeholder="Date" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGroupGrade">
+            <Form.Control type="number" placeholder="Grade" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGroupUrl">
             <Form.Control type="text" placeholder="Url" />
           </Form.Group>
           <Button>Add</Button>
