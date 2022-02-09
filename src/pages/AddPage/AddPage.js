71a27fcaf38ef878ecd5c30d937128b17443548d
Jakub Rzepiel <jakub.rzepiel@uczen.ckziu.net>
2022-02-09 21:22:32 +0100

    working database addition

--- src/pages/AddPage/AddPage.js
+++ src/pages/AddPage/AddPage.js
@@ -12,7 +12,6 @@ function AddPage() {
   const [newGrade, setNewGrade] = useState("");
   const [newUrl, setNewUrl] = useState("");
 
-  // test
 
   const add = () => {
     axios({
@@ -25,30 +24,33 @@ function AddPage() {
         url: newUrl,
       },
       withCredentials: true,
-      url: "http://localhost:3000/add-review"
-    }).then((res) => {});
+      url: "http://localhost:4000/add-review"
+    }).then((res) => {
+
+    });
   };
+
   return (
     <div className="add-page">
       <div className="form-box">
         <h1>Add new review</h1>
         <Form>
           <Form.Group className="mb-3" controlId="formGroupTitle">
-            <Form.Control type="text" placeholder="Title" />
+            <Form.Control type="text" placeholder="Title" onChange={(e) => setNewTitle(e.target.value)} />
           </Form.Group>
-          <Form.Group className="mb-7" controlId="formGroupContent">
-            <Form.Control type="text" placeholder="Content" />
+          <Form.Group className="mb-3" controlId="formGroupContent">
+            <Form.Control type="text" placeholder="Content" onChange={(e) => setNewContent(e.target.value)}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formGroupDate">
-            <Form.Control type="date" placeholder="Date" />
+            <Form.Control type="date" placeholder="Date" onChange={(e) => setNewDate(e.target.value)}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formGroupGrade">
-            <Form.Control type="number" placeholder="Grade" />
+            <Form.Control type="number" placeholder="Grade" onChange={(e) => setNewGrade(e.target.value)}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formGroupUrl">
-            <Form.Control type="text" placeholder="Url" />
+            <Form.Control type="text" placeholder="Url" onChange={(e) => setNewUrl(e.target.value)}/>
           </Form.Group>
-          <Button>Add</Button>
+          <Button onClick={add}>Add</Button>
         </Form>
       </div>
     </div>
