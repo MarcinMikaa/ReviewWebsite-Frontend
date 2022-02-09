c170b9abc28707949bd8adcaef2bc0876f97b9f0
Jan Piechota <jas.piechota18@gmail.com>
2022-02-09 20:37:43 +0100

    Added carousel

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,9 +1,27 @@
 import "./MainPage.css";
+import { Carousel } from "react-bootstrap";
+import kot from "../../images/kot.jpeg";
+import kot2 from "../../images/kot2.jpeg";
 
 function MainPage() {
   return (
-    <div className="main-page">
-      <h1>Main Page</h1>
+    <div className="carousel-comp">
+      <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src={kot} alt="First slide"/>
+        <Carousel.Caption>
+          <h2>First slide</h2>
+          <p>kjdhkdgsafgdjhfdsj</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={kot2} alt="First slide"/>
+        <Carousel.Caption>
+          <h2>First slide</h2>
+          <p>kjdhkdgsafgdjhfdsj</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     </div>
   );
 }
