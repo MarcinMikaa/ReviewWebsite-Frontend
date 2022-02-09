2a7b04c655d8479f8f9a158edb5d1dd83664c704
Jan Piechota <jas.piechota18@gmail.com>
2022-02-09 22:27:46 +0100

    Changed carousel images

--- src/components/CarouselComp/CarouselComp.js
+++ src/components/CarouselComp/CarouselComp.js
@@ -1,28 +1,29 @@
 import "./CarouselComp.css";
 import { Carousel } from "react-bootstrap";
-import kot from "../../images/kot.jpeg";
-import kot2 from "../../images/kot2.jpeg";
+import mountain from "../../images/mountain.jpg";
+import mountain2 from "../../images/mountain2.jpg";
+import mountain3 from "../../images/mountain3.jpg";
 
 function CarouselComp() {
     return (
       <div className="carousel-comp">
         <Carousel>
         <Carousel.Item>
-          <img className="carousel-image" src={kot} alt="First slide"/>
+          <img className="carousel-image" src={mountain} alt="First slide"/>
           <Carousel.Caption>
             <h2 className="carousel-header">First slide</h2>
             <p></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="carousel-image" src={kot2} alt="Second slide"/>
+          <img className="carousel-image" src={mountain2} alt="Second slide"/>
           <Carousel.Caption>
             <h2 className="carousel-header">Second slide</h2>
             <p></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="carousel-image" src={kot} alt="Third slide"/>
+          <img className="carousel-image" src={mountain3} alt="Third slide"/>
           <Carousel.Caption>
             <h2 className="carousel-header">Third slide</h2>
             <p></p>
