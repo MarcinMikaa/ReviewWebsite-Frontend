c6da4acffddc5e3aa28163e9feea083aab5323d9
Coverek <pawelzambrzycki03@gmail.com>
2022-03-31 11:10:02 +0200

    section comment added

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -1,8 +1,12 @@
 import "./ReviewCard.css";
-import { Col, Card } from "react-bootstrap";
+import { Col, Card, Form } from "react-bootstrap";
+import { DateTime } from "luxon";
 
 function ReviewCard({ id, title, content, date, grade, url }) {
-  // const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null;
+  const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null;
+
+
+
 
   return (
     <Col>
@@ -14,7 +18,20 @@ function ReviewCard({ id, title, content, date, grade, url }) {
         {/* <Card.Img variant="top" src={url} /> */}
         <div className="review-card-content">
           <Card.Text>{content}</Card.Text>
-          <Card.Text>{grade}</Card.Text>
+          <Card.Text className="grade">Grade: {grade}</Card.Text>
+        </div>
+        <hr/>
+        <div className="review-card-comment-section">
+        {user ?
+        <Form>
+          <h3>Comments section:</h3>
+          <Form.Group className="mb-3" controlId="formGroupTitle">
+            <Form.Control type="text" placeholder="Write something..." className="comment"  />
+          </Form.Group>
+        </Form>
+        :
+         <h2>Login to add a comment</h2>
+         }
         </div>
       </Card>
     </Col>
